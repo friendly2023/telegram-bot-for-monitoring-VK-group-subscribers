7c9959e6ff06f6e3679795e8b659c0da7e32c942
Vladimir <kazimirov.2013@mail.ru>
2023-09-16 23:50:06 +0300

    реализация ввода команд

--- bot telegram/index.js
+++ bot telegram/index.js
@@ -2,15 +2,21 @@ const token = '6510713266:AAEaFfeGn4Kna6hs7qGf76GZD6bRlfR_SA8'
 const TelegramApi = require('node-telegram-bot-api')
 const bot = new TelegramApi(token,{polling: true})
 
+bot.setMyCommands([
+    {command: '/start', description: 'Начальное приветствие'},
+    {command: '/list', description: 'Список сообществ'},
+])
+
 bot.on('message', msg =>{
     //console.log(msg)
     const text = msg.text;
     const chatId = msg.chat.id;
 
     if(text === '/start') {
-        bot.sendMessage(chatId, `Добро пожаловать ${msg.from.first_name}`);
+        return bot.sendMessage(chatId, `Добро пожаловать ${msg.from.first_name}`);
     }
-    if(text === '/info') {
-        bot.sendMessage(chatId, `Список сообществ: `);
+    if(text === '/list') {
+        return bot.sendMessage(chatId, `Список сообществ: `);
     }
+    return bot.sendMessage(chatId, `Хз о чем ты..`);
 })
