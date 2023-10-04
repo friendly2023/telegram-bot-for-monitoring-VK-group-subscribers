9c38d7fc2d1182fe37424f407bc90a8aee87e77b
Vladimir <kazimirov.2013@mail.ru>
2023-10-04 19:45:40 +0300

    key+сообщества

---
+++ serviceKey/telegramKey.js
@@ -0,0 +1,2 @@
+const token = "6510713266:AAEaFfeGn4Kna6hs7qGf76GZD6bRlfR_SA8";
+exports.token = token;
---
+++ serviceKey/vkKey.js
@@ -0,0 +1,2 @@
+const serviceKey = "cbd1a557cbd1a557cbd1a557fac8c436bdccbd1cbd1a557af2df0271cf1f9fc163446e5";
+exports.serviceKey = serviceKey;
