7ae246d23f971813f8a1b646d26282d049ac19a3
Vladimir <kazimirov.2013@mail.ru>
2023-10-06 23:20:00 +0300

    добавлена функция запроса имени сообщества

--- bot_VK_get/bot.js
+++ bot_VK_get/bot.js
@@ -63,6 +63,26 @@ async function getNewGroupMembersData(groupId) {
         .then(response => response.json())
 }
 
+async function getCommunityName(groupId) {
+    return fetch("https://api.vk.com/method/groups.getById", {
+        "headers": {
+            "content-type": "application/x-www-form-urlencoded",
+            "accept": "*/*",
+            "accept-language": "ru-RU,ru;q=0.9,en-GB;q=0.8,en;q=0.7,en-US;q=0.6",
+        },
+        "body": `group_id=${groupId}&access_token=${serviceKey}&v=5.131&fields=members_count`,
+        "method": "POST"
+    })
+        .then(response => response.json())
+        .then(data => {
+            const communityName = data.response[0].name;
+            return `${communityName}`;
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+}
+
 function getOldGroupMembersDataFromFile(path) {
     return getMembersIds(readFile(path))
 }
