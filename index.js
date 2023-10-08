0f6392e5e50d888718927da172c42ab83cd14f19
Vladimir <kazimirov.2013@mail.ru>
2023-10-08 21:06:50 +0300

    списки сообществ индивидуальны под пользователя

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function outputMessage() {
         }
         if (text === '/communities') {
             return await bot.sendMessage(chatId, `Список сообществ: `,
-                await creatureArrayCommunities());
+                await creatureArrayCommunities(chatId));
         }
         if (text === '/new') {
             return await bot.sendMessage(chatId, `Введите ID сообщества в формате:
@@ -41,7 +41,7 @@ function outputMessage() {
         }
         if (text.slice(0, 3) == 'ID:') {
             groupId = text.slice(3);
-            return await communitiesUtils.addingNewCommunity(groupId)
+            return await communitiesUtils.addingNewCommunity(groupId, chatId)
                 .then(result => bot.sendMessage(chatId, `${result}`));
         }
         return await bot.sendMessage(chatId, `Хз о чем ты..`);
@@ -50,23 +50,24 @@ function outputMessage() {
     bot.on('callback_query', async msg => {//ответ на кнопку '/communities'
         const groupId = msg.data;
         const chatId = msg.message.chat.id;
-        const result = await communitiesUtils.compareMembersData(groupId);
+        const result = await communitiesUtils.compareMembersData(groupId,chatId);
         return await bot.sendMessage(chatId, `${result}`);
     })
 }
 
-async function creatureArrayCommunities() {//подключение для кнопок '/communities'
-    return { reply_markup: { inline_keyboard: await searchFileTarget() } }
+async function creatureArrayCommunities(chatId) {//подключение для кнопок '/communities'
+    return { reply_markup: { inline_keyboard: await searchFileTarget(chatId) } }
 }
 
-async function searchFileTarget() {//поиск файлов в папке+генерация массива для кнопки
+async function searchFileTarget(chatId) {//поиск файлов в папке+генерация массива для кнопки
     let communitiesList = fs.readdirSync(fileTarget)
     let buttonsArray = []
     for (c of communitiesList) {
-        communityId = c.slice(0, -5)
-        //console.log(communityId)
-        cName = await communitiesUtils.getCommunityName(communityId)
-        buttonsArray.push([{ text: cName, callback_data: communityId }])
+        if (c.split('+,+')[0] == chatId) {
+            communityId = c.split('+,+')[1].slice(0, -5);
+            cName = await communitiesUtils.getCommunityName(communityId)
+            buttonsArray.push([{ text: cName, callback_data: communityId }])
+        }
     }
     return buttonsArray
 }
