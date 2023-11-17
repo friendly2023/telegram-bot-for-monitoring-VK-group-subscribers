43fecbff6b22cf853449a29147f97026a74edad7
Vladimir <kazimirov.2013@mail.ru>
2023-11-17 20:18:02 +0300

    запись ТОЛЬКО уникальных id пользователей из ТГ

--- database/writingToTableDB.js
+++ database/writingToTableDB.js
@@ -13,100 +13,114 @@ let db = new sqlite3.Database('./database/vkDB.db', (err) => {
 // writingToCommunities('grut', 'название грут')
 // writingToUsersToCommunities(123456, 'grut6')
 // writingToCommunitiesList('gruk7t', 'rtuhgrt5h;ljh')
-exports.writingToUsers = writingToUsers;
-exports.writingToCommunities = writingToCommunities;
-exports.writingToUsersToCommunities = writingToUsersToCommunities;
-exports.writingToCommunitiesList = writingToCommunitiesList;
+
+// exports.writingToUsers = writingToUsers;
+// exports.writingToCommunities = writingToCommunities;
+// exports.writingToUsersToCommunities = writingToUsersToCommunities;
+// exports.writingToCommunitiesList = writingToCommunitiesList;
 
 exports.writeToFileSQL = writeToFileSQL;
 
 async function writeToFileSQL(telegramId, firstName, communityId) {
   let newDataGroup = await communitiesUtils.getNewGroupMembersData(communityId);
+  console.log(newDataGroup)
   let newNameGroup = await communitiesUtils.getCommunityName(communityId);
-  let dataInSQL = await writeToSQL(telegramId, firstName, newNameGroup, communityId, newDataGroup);
+  console.log(newNameGroup)
+  let dataInSQL = await writeToSQL(telegramId, firstName);
   return `Сообщество добавлено в ДБ`
 }
 
-async function writeToSQL(telegramId, firstName, title, communityId, jsonFollowersList) {
-  var myDate = new Date(); 
+async function writeToSQL(telegramId, firstName) {
+  //var myDate = new Date(); 
+  let db = new sqlite3.Database('./database/vkDB.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+  });
   db.serialize(() => {
-    db.run(`INSERT INTO users
-            VALUES ('${telegramId}', '${firstName}')`)
-    db.run(`INSERT INTO communities
-            VALUES ('${communityId}', '${title}')`)
-    db.run(`INSERT INTO usersToCommunities
-            VALUES ('${telegramId}', '${communityId}')`)
-    db.run(`INSERT INTO communitiesList
-            VALUES ('${communityId}', '${myDate}', '${jsonFollowersList}')`)
+    
+    db.run(`INSERT INTO users (telegramId, firstName)
+            VALUES ('${telegramId}', '${firstName}')
+            ON CONFLICT(telegramId) DO UPDATE SET
+            firstName='${firstName}';`)
+    
+    // db.run(`INSERT INTO users
+    //         VALUES ('${telegramId}', '${firstName}')`)
+    // db.run(`INSERT INTO communities
+    //         VALUES ('${communityId}', '${title}')`)
+    // db.run(`INSERT INTO usersToCommunities
+    //         VALUES ('${telegramId}', '${communityId}')`)
+    // db.run(`INSERT INTO communitiesList
+    //         VALUES ('${communityId}', '${myDate}', '${jsonFollowersList}')`)
   })
   db.close((err) => {
     if (err) {
-      return console.error(err.message);
+      console.error(err.message);
     }
   });
 }
 
-async function writingToUsers(telegramId, firstName) {
-  let sql = `INSERT INTO users
-             VALUES ('${telegramId}', '${firstName}')`
+// async function writingToUsers(telegramId, firstName) {
+//   let sql = `INSERT INTO users
+//              VALUES ('${telegramId}', '${firstName}')`
 
-  db.serialize(() => {
-    db.each(sql, (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-    });
-  });
+//   db.serialize(() => {
+//     db.each(sql, (err) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//     });
+//   });
 
-  // закрытие бд
-  db.close();
-}
+//   // закрытие бд
+//   db.close();
+// }
 
-async function writingToCommunities(communityId, title) {
-  let sql = `INSERT INTO communities
-             VALUES ('${communityId}', '${title}')`
+// async function writingToCommunities(communityId, title) {
+//   let sql = `INSERT INTO communities
+//              VALUES ('${communityId}', '${title}')`
 
-  db.serialize(() => {
-    db.each(sql, (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-    });
-  });
+//   db.serialize(() => {
+//     db.each(sql, (err) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//     });
+//   });
 
-  // закрытие бд
-  db.close();
-}
+//   // закрытие бд
+//   db.close();
+// }
 
-async function writingToUsersToCommunities(telegramId, communityId) {
-  let sql = `INSERT INTO usersToCommunities
-             VALUES ('${telegramId}', '${communityId}')`
+// async function writingToUsersToCommunities(telegramId, communityId) {
+//   let sql = `INSERT INTO usersToCommunities
+//              VALUES ('${telegramId}', '${communityId}')`
 
-  db.serialize(() => {
-    db.each(sql, (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-    });
-  });
+//   db.serialize(() => {
+//     db.each(sql, (err) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//     });
+//   });
 
-  // закрытие бд
-  db.close();
-}
+//   // закрытие бд
+//   db.close();
+// }
 
-async function writingToCommunitiesList(communityId, jsonFollowersList) {
-  var myDate = new Date(); 
-  let sql = `INSERT INTO communitiesList
-             VALUES ('${communityId}', '${myDate}', '${jsonFollowersList}')`
+// async function writingToCommunitiesList(communityId, jsonFollowersList) {
+//   var myDate = new Date(); 
+//   let sql = `INSERT INTO communitiesList
+//              VALUES ('${communityId}', '${myDate}', '${jsonFollowersList}')`
 
-  db.serialize(() => {
-    db.each(sql, (err) => {
-      if (err) {
-        console.error(err.message);
-      }
-    });
-  });
+//   db.serialize(() => {
+//     db.each(sql, (err) => {
+//       if (err) {
+//         console.error(err.message);
+//       }
+//     });
+//   });
 
-  // закрытие бд
-  db.close();
-}
+//   // закрытие бд
+//   db.close();
+// }
