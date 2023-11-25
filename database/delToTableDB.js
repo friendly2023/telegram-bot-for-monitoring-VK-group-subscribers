fd9ee8af890b19fac6532321afb68225892f2b5e
Vladimir <kazimirov.2013@mail.ru>
2023-11-25 16:46:24 +0300

    можно удалить сообщество

--- database/delToTableDB.js
+++ database/delToTableDB.js
@@ -8,7 +8,27 @@ let db = new sqlite3.Database('./database/vkDB.db', (err) => {
 });
 
 exports.delToFileSQL = delToFileSQL;
+// (async () => console.log(await delToFileSQL('412993464', 'public222303599')))()
 
-async function delToFileSQL() {
-    
+async function delToFileSQL(chatId, groupId) {
+    let delCommunities= await delToSQL(chatId, groupId)
+    return `Сообщество "${groupId}" удалено`    
+}
+
+async function delToSQL(telegramId, communityId) {
+    let db = new sqlite3.Database('./database/vkDB.db', (err) => {
+        if (err) {
+            console.error(err.message);
+        }
+    });
+    db.serialize(() => {
+
+        db.run(`DELETE FROM usersToCommunities
+                WHERE telegramId='${telegramId}' and communityId='${communityId}';`)
+    })
+    db.close((err) => {
+        if (err) {
+            console.error(err.message);
+        }
+    });
 }
